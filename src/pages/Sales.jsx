04035b79cb06823c999ac3b487dbe17b163f6460
Kevin <kevin.esposado@gmail.com>
2023-03-23 15:23:04 +0800

    updated

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -15,8 +15,13 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { DataGrid } from '@mui/x-data-grid';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useAuthContext } from '../hooks/useAuthContext'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Alert } from '@mui/material';
 import { PaddingRounded } from '@mui/icons-material';
+
+
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 const theme = createTheme({
      palette: {
           neutral: {
@@ -80,13 +85,109 @@ const SearchContainer = styled.div`
      padding-bottom: 30px;
 `
 
-/**GET REQUESTS */
-
-
 const Sales = (props) => {
-    
 
-   
+     const { user } = useAuthContext()
+     const [date_from, setdate_from] = useState(() => {
+          const date = new Date();
+          let day = date.getDate().toString().padStart(2, '0');;
+          let month = (date.getMonth() + 1).toString().padStart(2, '0');;
+          let year = date.getFullYear();
+          let currentDate = `${month}-${day}-${year}`;
+          return currentDate
+     })
+     const [date_to, setdate_to] = useState(() => {
+          const date = new Date();
+          let day = date.getDate().toString().padStart(2, '0');;
+          let month = (date.getMonth() + 1).toString().padStart(2, '0');;
+          let year = date.getFullYear();
+          let currentDate = `${month}-${day}-${year}`;
+          return currentDate
+     })
+     const [filtered_data, setfiltered_data] = useState([])
+
+     const [sales, setsales] = useState([])
+     useEffect(() => {
+          const fetchSales = async () => {
+               const response = await fetch('https://coop-backend-v1.herokuapp.com/api/pos', {
+                    headers: {
+                         'Authorization': `Bearer ${user.token}`
+                    }
+               })
+               const json = await response.json()
+
+               if (response.ok) {
+                    const filteredData = json.filter(item => {
+                         const date = item.pos_date
+                         return date >= date_from && date <= date_to
+
+                    });
+                    //setfiltered_data(filteredData)
+                    //setsales(json)
+                    setsales(filteredData)
+               }
+          }
+          if (user) {
+               fetchSales();
+          }
+
+     }, [date_to, date_from])
+
+
+     const convertDateToStringFrom = (date) => {
+          const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+          const dateString = new Date(date).toLocaleDateString('en-US', options).replace(/\//g, '-');
+          setdate_from(dateString)
+     };
+     const convertDateToStringTo = (date) => {
+          const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+          const dateString = new Date(date).toLocaleDateString('en-US', options).replace(/\//g, '-');
+          setdate_to(dateString)
+     };
+
+     function getRowHeight(params) {
+          const lineHeight = 20; // Set the desired line height
+          const itemsHeight = params.value?.length * lineHeight || 0;
+          const padding = 120; // Set the desired padding
+          return itemsHeight + padding;
+        }
+
+     const columns = [
+          { field: 'pos_date', headerName: 'Date', width: 150 },
+          { field: 'pos_transaction_id', headerName: 'Transaction ID', width: 200 },
+          { field: 'pos_total', headerName: 'Total', width: 150 },
+          {
+               field: 'pos_items',
+               headerName: 'Items',
+               width: 1000,
+               renderCell: (params) => {
+                    return (
+                         <table>
+                              <thead>
+                                   <tr>
+                                        <th style={{ paddingRight: "200px" }}>Name</th>
+                                        <th style={{ paddingRight: "40px" }}>Quantity</th>
+                                        <th style={{ paddingRight: "40px" }}>Price</th>
+                                        <th style={{ paddingRight: "40px" }}>Total</th>
+                                   </tr>
+                              </thead>
+                              <tbody>
+                                   {params.value.map((item, index) => (
+                                        <tr key={index}>
+                                             <td>{item.product_name}</td>
+                                        
+                                             <td>{item.product_quantity}</td>
+                                             <td>{item.product_selling_price}</td>
+                                             <td>PHP {item.product_total}</td>
+                                        </tr>
+                                   ))}
+                              </tbody>
+                         </table>
+                    );
+               },
+          },
+     ];
+
 
      return (
           <div style={{ display: "flex" }}>
@@ -96,7 +197,37 @@ const Sales = (props) => {
                          <Main>
                               <Header title={props.title} user={props.user} />
                               <Card>
-                                   This will be available after system update later today :)
+                                   <div style={{ display: "flex" }}>
+                                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                             <DatePicker
+                                                  label="Date From"
+                                                  value={date_from}
+                                                  inputFormat="MM-DD-YYYY"
+                                                  onChange={convertDateToStringFrom}
+                                                  renderInput={(params) => <TextField fullWidth required style={{ paddingBottom: "20px", marginRight: "20px" }}{...params} error={false} />}
+                                             />
+                                             <DatePicker
+                                                  label="Date To"
+                                                  value={date_to}
+                                                  inputFormat="MM-DD-YYYY"
+                                                  onChange={convertDateToStringTo}
+                                                  renderInput={(params) => <TextField fullWidth required style={{ paddingBottom: "20px" }}{...params} error={false} />}
+                                             />
+
+                                        </LocalizationProvider>
+
+                                   </div>
+                                   <div style={{ height: 475, width: '100%' }}>
+                                        <DataGrid
+                                             getRowId={(row) => row._id}
+                                             rows={sales}
+                                             columns={columns}
+                                             pageSize={7}
+                                             rowsPerPageOptions={[5]}
+                                             getRowHeight={getRowHeight}
+                                        //onRowClick={handleRowClick}
+                                        />
+                                   </div>
                               </Card>
                          </Main>
                     </Wrapper>
